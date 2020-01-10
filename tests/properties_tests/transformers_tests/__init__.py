7ad62fb26a9533a522dcc2f94abdd7528bd3dd12
David Shriver <dls2fc@virginia.edu>
2020-01-10 12:35:05 -0500

    add a few more tests

--- tests/properties_tests/transformers_tests/__init__.py
+++ tests/properties_tests/transformers_tests/__init__.py
@@ -1,6 +1,7 @@
 import unittest
 
 from .propagate_constants_tests import *
+from .to_cnf_tests import *
 
 if __name__ == "__main__":
     unittest.main()
--- tests/properties_tests/transformers_tests/to_cnf_tests.py
+++ tests/properties_tests/transformers_tests/to_cnf_tests.py
@@ -4,7 +4,103 @@ from dnnv.properties import *
 
 
 class ToCNFTests(unittest.TestCase):
-    pass
+    def test_and(self):
+        a = Symbol("a")
+        b = Symbol("b")
+        c = Symbol("c")
+
+        expr = a & b & c
+        cnf_expr = expr.to_cnf()
+        self.assertEqual(str(expr), "(a & b & c)")
+        self.assertEqual(str(cnf_expr), "(a & b & c)")
+
+        expr = a & ~b & c
+        cnf_expr = expr.to_cnf()
+        self.assertEqual(str(expr), "(a & ~b & c)")
+        self.assertEqual(str(cnf_expr), "(a & ~b & c)")
+
+    def test_or(self):
+        a = Symbol("a")
+        b = Symbol("b")
+        c = Symbol("c")
+
+        expr = a | b | c
+        cnf_expr = expr.to_cnf()
+        self.assertEqual(str(expr), "(a | b | c)")
+        self.assertEqual(str(cnf_expr), "((a | b | c))")
+
+        expr = a | ~b | c
+        cnf_expr = expr.to_cnf()
+        self.assertEqual(str(expr), "(a | ~b | c)")
+        self.assertEqual(str(cnf_expr), "((a | ~b | c))")
+
+    def test_from_cnf(self):
+        a = Symbol("a")
+        b = Symbol("b")
+        c = Symbol("c")
+
+        expr = (a | b | c) & (~a | b)
+        cnf_expr = expr.to_cnf()
+        self.assertEqual(str(expr), "((a | b | c) & (~a | b))")
+        self.assertEqual(str(cnf_expr), "((a | b | c) & (~a | b))")
+
+        expr = (a | ~b | c) & (~a | b | ~c)
+        cnf_expr = expr.to_cnf()
+        self.assertEqual(str(expr), "((a | ~b | c) & (~a | b | ~c))")
+        self.assertEqual(str(cnf_expr), "((a | ~b | c) & (~a | b | ~c))")
+
+    def test_from_dnf(self):
+        a = Symbol("a")
+        b = Symbol("b")
+        c = Symbol("c")
+
+        expr = (a & b & ~c) | (~a & b)
+        cnf_expr = expr.to_cnf()
+        self.assertEqual(str(expr), "((a & b & ~c) | (~a & b))")
+        self.assertEqual(
+            str(cnf_expr),
+            "((a | ~a) & (a | b) & (b | ~a) & (b | b) & (~c | ~a) & (~c | b))",
+        )
+
+        expr = (a & ~b) | (~a & b & ~c)
+        cnf_expr = expr.to_cnf()
+        self.assertEqual(str(expr), "((a & ~b) | (~a & b & ~c))")
+        self.assertEqual(
+            str(cnf_expr),
+            "((a | ~a) & (a | b) & (a | ~c) & (~b | ~a) & (~b | b) & (~b | ~c))",
+        )
+
+    def test_qf(self):
+        a = Symbol("a")
+        b = Symbol("b")
+        c = Symbol("c")
+
+        expr = ((Implies(a, (b & ~c)) & Implies(~a, (~b & ~c))) & (c | a)) | (
+            a & ~b & ~c
+        )
+        cnf_expr = expr.to_cnf()
+        self.assertEqual(
+            str(expr),
+            "(((a ==> (b & ~c)) & (~a ==> (~b & ~c)) & (c | a)) | (a & ~b & ~c))",
+        )
+        self.assertEqual(
+            str(cnf_expr),
+            "((b | ~a | a) & (b | ~a | ~b) & (b | ~a | ~c) "
+            "& (~c | ~a | a) & (~c | ~a | ~b) & (~c | ~a | ~c) "
+            "& (~b | a | a) & (~b | a | ~b) & (~b | a | ~c) "
+            "& (~c | a | a) & (~c | a | ~b) & (~c | a | ~c) "
+            "& (c | a | a) & (c | a | ~b) & (c | a | ~c))",
+        )
+
+    def test_forall(self):
+        x = Symbol("x")
+
+        expr = Forall(x, Implies((x > 0) & (x < 10), 2 * x < 20))
+        cnf_expr = expr.to_cnf()
+        self.assertEqual(
+            str(expr), "Forall(x, (((x > 0) & (x < 10)) ==> ((2 * x) < 20)))"
+        )
+        self.assertEqual(str(cnf_expr), "(((x <= 0) | (x >= 10) | ((2 * x) < 20)))")
 
 
 if __name__ == "__main__":
