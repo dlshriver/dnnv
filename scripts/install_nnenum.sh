e86e5a2a6462dc75aa3c42c91af88c0e665f6ce3
David Shriver <dls2fc@virginia.edu>
2020-09-02 14:39:33 -0400

    nnenum mvp

---
+++ scripts/install_nnenum.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+PROJECT_DIR=${PROJECT_DIR:-$(
+    cd $(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)/..
+    pwd
+)}
+source $PROJECT_DIR/scripts/install_common.sh
+
+cd $PROJECT_DIR/lib
+git clone https://github.com/stanleybak/nnenum.git
+cd nnenum
+git checkout f853227
+git pull
+
+pip install "threadpoolctl>=2.1,<2.2"
+pip install "swiglpk>=4.65,<4.66"
+pip install "foolbox>=2.4,<2.5"
+pip install "skl2onnx>=1.7,<1.8"
