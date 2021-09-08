c6b09d1caee9470f9b17ad46a3c8689b619ba145
David Shriver <dls2fc@virginia.edu>
2021-09-08 12:03:59 -0400

    add new test for reduction

--- new_tests/unit_tests/test_verifiers/test_common/test_reductions/test_iopolytope/test_IOPolytopeReduction.py
+++ new_tests/unit_tests/test_verifiers/test_common/test_reductions/test_iopolytope/test_IOPolytopeReduction.py
@@ -33,17 +33,25 @@ def test_non_concrete_network():
 
 
 # TODO : finish this
-# def test_simple_property():
-#     reduction = IOPolytopeReduction()
+def test_simple_property():
+    reduction = IOPolytopeReduction()
 
-#     phi = Exists(Symbol("x"), Network("N")(Symbol("x")) > Constant(0))
-#     input_op = operations.Input((1,), np.dtype(np.float64))
-#     output_op = operations.Add(input_op, operations.Mul(np.float64(-2), input_op))
-#     op_graph = OperationGraph([output_op])
-#     phi.concretize(N=op_graph)
+    phi = Exists(Symbol("x"), Network("N")(Symbol("x")) > Constant(0))
+    input_op = operations.Input((1,), np.dtype(np.float64))
+    output_op = operations.Add(input_op, operations.Mul(np.float64(-2), input_op))
+    op_graph = OperationGraph([output_op])
+    phi.concretize(N=op_graph)
 
-#     properties = list(reduction.reduce_property(phi))
-#     print(properties)
-#     for p in properties:
-#         print(p)
-#     assert 0
+    properties = list(reduction.reduce_property(phi))
+    assert len(properties) == 1
+    prop = properties[0]
+
+    assert len(prop.networks) == 1
+
+    assert np.all(prop.input_constraint._lower_bound == np.array([-np.inf]))
+    assert np.all(prop.input_constraint._upper_bound == np.array([np.inf]))
+
+    assert np.all(
+        prop.output_constraint._lower_bound == np.array([np.nextafter(0, 1)])
+    )
+    assert np.all(prop.output_constraint._upper_bound == np.array([np.inf]))
