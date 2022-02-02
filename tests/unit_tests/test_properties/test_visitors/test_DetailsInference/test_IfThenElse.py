dc2b3bc5a5725c2271d108b52df3b518fb789d54
David Shriver <davidshriver@outlook.com>
2022-02-02 00:40:11 -0500

    fix: be more permissive in IfThenElse result types
    Refs: #75

--- tests/unit_tests/test_properties/test_visitors/test_DetailsInference/test_IfThenElse.py
+++ tests/unit_tests/test_properties/test_visitors/test_DetailsInference/test_IfThenElse.py
@@ -61,17 +61,15 @@ def test_IfThenElse_constant_true_expr():
 
     assert inference.types[c].is_concrete
     assert inference.types[t].is_concrete
-    assert inference.types[f].is_concrete
-    assert inference.types[expr].is_concrete
+    assert not inference.types[f].is_concrete
+    assert not inference.types[expr].is_concrete
 
     assert inference.shapes[c].value == ()
     assert inference.types[c].value == bool
     assert inference.shapes[t].value == t.value.shape
     assert inference.types[t].value == t.value.dtype
     assert inference.shapes[f].value == t.value.shape
-    assert inference.types[f].value == t.value.dtype
     assert inference.shapes[expr].value == t.value.shape
-    assert inference.types[expr].value == t.value.dtype
 
 
 def test_IfThenElse_constant_false_expr():
@@ -87,18 +85,45 @@ def test_IfThenElse_constant_false_expr():
     assert inference.shapes[expr].is_concrete
 
     assert inference.types[c].is_concrete
+    assert not inference.types[t].is_concrete
+    assert inference.types[f].is_concrete
+    assert not inference.types[expr].is_concrete
+
+    assert inference.shapes[c].value == ()
+    assert inference.types[c].value == bool
+    assert inference.shapes[t].value == f.value.shape
+    assert inference.shapes[f].value == f.value.shape
+    assert inference.types[f].value == f.value.dtype
+    assert inference.shapes[expr].value == f.value.shape
+
+
+def test_IfThenElse_constant_true_false_expr():
+    inference = DetailsInference()
+
+    c, t, f = Symbol("c"), Constant(1), Constant(np.array(1.0))
+    expr = IfThenElse(c, t, f)
+    inference.visit(expr)
+
+    assert inference.shapes[c].is_concrete
+    assert inference.shapes[t].is_concrete
+    assert inference.shapes[f].is_concrete
+    assert inference.shapes[expr].is_concrete
+
+    assert inference.types[c].is_concrete
     assert inference.types[t].is_concrete
     assert inference.types[f].is_concrete
     assert inference.types[expr].is_concrete
 
     assert inference.shapes[c].value == ()
     assert inference.types[c].value == bool
-    assert inference.shapes[t].value == f.value.shape
-    assert inference.types[t].value == f.value.dtype
+    assert inference.shapes[t].value == ()
+    assert inference.types[t].value == np.min_scalar_type(t.value)
     assert inference.shapes[f].value == f.value.shape
     assert inference.types[f].value == f.value.dtype
     assert inference.shapes[expr].value == f.value.shape
-    assert inference.types[expr].value == f.value.dtype
+    assert inference.types[expr].value == np.result_type(
+        np.min_scalar_type(t.value), f.value.dtype
+    )
 
 
 def test_IfThenElse_incompatible_shapes():
@@ -123,16 +148,6 @@ def test_IfThenElse_incompatible_shapes():
 
 def test_IfThenElse_incompatible_types():
     inference = DetailsInference()
-
-    with get_context():
-        c, t, f = (
-            Symbol("c"),
-            Constant(np.random.rand(3, 5)),
-            Constant(np.random.rand(3, 5) > 0.5),
-        )
-        expr = IfThenElse(c, t, f)
-        with pytest.raises(DNNVTypeError):
-            inference.visit(expr)
 
     with get_context():
         c, t, f = Constant(8), Symbol("true"), Symbol("false")
